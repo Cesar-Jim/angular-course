ccefe941d69f123045319bc3bbaff913911057c0
Cesar Jimenez <cijgr@hotmail.com>
2019-11-25 12:51:14 -0600

    project 06 completed

---
+++ 06-miscelaneos/src/app/components/home/home.component.ts
@@ -0,0 +1,72 @@
+import { RouterModule, Routes } from '@angular/router';
+import { AfterViewChecked } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  OnChanges,
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked,
+  OnDestroy
+} from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  template: `
+    <app-ng-style></app-ng-style>
+
+    <app-css></app-css>
+
+    <app-clases></app-clases>
+
+    <!-- directiva personalizada: -->
+    <p [appResaltado]="'blue'">
+        Hola mundo.
+    </p>
+
+    <!-- directiva ngSwitch: -->
+    <app-ng-switch></app-ng-switch>
+  `,
+  styles: []
+})
+export class HomeComponent implements OnInit, OnChanges, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
+
+  constructor() {
+    console.log('Constructor');
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngOnChanges() {
+    console.log('ngOnChanges');
+  }
+
+  ngDoCheck() {
+    console.log("ngDoCheck");
+  }
+
+  ngAfterContentInit() {
+    console.log("ngAfterContentInit");
+  }
+
+  ngAfterContentChecked() {
+    console.log("ngAfterContentChecked");
+  }
+
+  ngAfterViewInit() {
+    console.log("ngAfterViewInit");
+  }
+
+  ngAfterViewChecked() {
+    console.log("ngAfterViewChecked");
+  }
+
+  ngOnDestroy() {
+    console.log("ngOnDestroy");
+  }
+
+}
